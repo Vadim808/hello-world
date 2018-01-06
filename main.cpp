dc26bb3ae1f08d7e609df3eab3f65976ce67dd1b
Vadim808 <vch.98@mail.ru>
2018-01-06 17:21:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
 #include <vector>
+#include <cstdint>
 
-using namespace std;
-
+template <typename T>
 struct node {
-    int64_t value;
+    T value;
     node * next;
 };
 
 class my_list {
 public:
-    node * begin = new node;
-    my_list(vector<int64_t> numbers) {
+    node<int64_t> * begin = new node<int64_t>;
+    my_list(std::vector<int64_t> numbers) {
         begin->value = numbers[0];
-        node * second_x = new node;
+        node<int64_t> * second_x = new node<int64_t>;
         second_x->value = numbers[1];
         begin->next = second_x;
         for (int i = 1; i < numbers.size() - 1; ++i) {
-            node * x = new node;
+            node<int64_t> * x = new node<int64_t>;
             x->value = numbers[i + 1];
             second_x->next = x;
             second_x = x;
@@ -28,42 +25,45 @@ public:
     }
 };
 
-void merge(int64_t left, int64_t middle, int64_t right, node * l, node * mi) {
+void merge(int64_t left, int64_t middle, int64_t right, node<int64_t> * l, node<int64_t> * mi) {
     int64_t n1 = middle - left + 1;
     int64_t n2 = right - middle;
-    vector<int64_t> L;
-    vector<int64_t> R;
-    L.resize(n1 + 1);
-    R.resize(n2 + 1);
-    node * k = l;
+    node<int64_t> * L = new node<int64_t>;
+    node<int64_t> * R = new node<int64_t>;
+    node<int64_t> * startL = L;
+    node<int64_t> * startR = R;
+    node<int64_t> * k = l;
     for (int64_t i = 0; i < n1; ++i) {
-        L[i] = l->value;
+        L->value = l->value;
         l = l->next;
+        L->next = new node<int64_t>;
+        L = L->next;
     }
+    L->value = 1e18 + 1;
     mi = mi->next;
     for (int j = 0; j < n2; ++j) {
-        R[j] = mi->value;
+        R->value = mi->value;
         mi = mi->next;
+        R->next = new node<int64_t>;
+        R = R->next;
     }
-    L[n1] = 1e18 + 1;
-    R[n2] = 1e18 + 1;
-    int64_t i = 0, j = 0;
+    R->value = 1e18 + 1;
     for (int64_t m = left; m <= right; ++m) {
-        if (L[i] <= R[j]) {
-            k->value = L[i];
-            i += 1;
+        if (startL->value <= startR->value) {
+            k->value = startL->value;
+            startL = startL->next;
         } else {
-            k->value = R[j];
-            j += 1;
+            k->value = startR->value;
+            startR = startR->next;
         }
         k = k->next;
     }
 }
 
-void merge_sort(int left, int right, node * l) {
+void merge_sort(int left, int right, node<int64_t> * l) {
     if (left < right) {
         int middle = (left + right) / 2;
-        node * m = l;
+        node<int64_t> * m = l;
         for (int64_t i = left; i < middle; ++i) {
             m = m->next;
         }
@@ -75,18 +75,18 @@ void merge_sort(int left, int right, node * l) {
 
 int main() {
     int N;
-    cin >> N;
-    vector<int64_t> numbers;
+    std::cin >> N;
+    std::vector<int64_t> numbers;
     numbers.resize(N);
     for (int i = 0; i < N; ++i) {
-        cin >> numbers[i];
+        std::cin >> numbers[i];
     }
     my_list list_numbers(numbers);
-    node * x = new node;
+    node<int64_t> * x = new node<int64_t>;
     x = list_numbers.begin;
     merge_sort(0, N - 1, x);
     for (int i = 0; i < N; ++i) {
-        cout << x->value << ' ';
+        std::cout << x->value << ' ';
         x = x->next;
     }
 }
